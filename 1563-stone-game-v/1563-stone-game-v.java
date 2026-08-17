392cd4770b313ee532652d9b650b7aa659d2e552
karthikeya Chandrapati <chandrapatikarthikeya2006@gmail.com>
2026-08-17 13:54:50 +0530

    Time: 357 ms (14.66%), Space: 47.7 MB (49.28%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int stoneGameV(int[] stoneValue) {
+        int n = stoneValue.length;
+        
+        // Compute prefix sums for O(1) range sum queries
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + stoneValue[i];
+        }
+        
+        // dp[i][j] stores max score for subarray stoneValue[i...j]
+        int[][] dp = new int[n][n];
+        
+        // Solve for lengths from 2 to n
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                
+                // Try all possible split points k where left part is stoneValue[i...k]
+                for (int k = i; k < j; k++) {
+                    int leftSum = prefixSum[k + 1] - prefixSum[i];
+                    int rightSum = prefixSum[j + 1] - prefixSum[k + 1];
+                    
+                    if (leftSum < rightSum) {
+                        dp[i][j] = Math.max(dp[i][j], leftSum + dp[i][k]);
+                    } else if (leftSum > rightSum) {
+                        dp[i][j] = Math.max(dp[i][j], rightSum + dp[k + 1][j]);
+                    } else { // leftSum == rightSum
+                        dp[i][j] = Math.max(dp[i][j], leftSum + Math.max(dp[i][k], dp[k + 1][j]));
+                    }
+                }
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+}
